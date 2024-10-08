a98b56d66694c45b04a451d1f194ce143f558c34
Evana Pradhan <evanapra@gmail.com>
2024-10-08 10:51:47 -0700

    Update dependencies and add new source code

---
+++ models/EventLog.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const EventLogSchema = new Schema({
+    eventType: String, // Type of event (click, hover, focus)
+    elementName: String, // Name of the element (e.g., SendButton)
+    timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('EventLog', EventLogSchema);
